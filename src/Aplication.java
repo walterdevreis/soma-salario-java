2cd7ccbb4c888fbb77199ea88fb53a48d57bd232
walterdevreis <walter-reis@outlook.com>
2024-01-07 21:49:58 -0300

    Project completed

--- src/Aplication.java
+++ src/Aplication.java
@@ -2,8 +2,14 @@ package src;
 
 import src.entities.Employee;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Aplication {
 
@@ -12,12 +18,43 @@ public class Aplication {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
-        Employee employee = new Employee("Bob", "bob@gmail.com", 3500.00);
+        System.out.print("Enter full file path: ");
+        String path = scanner.next();
 
-        System.out.println(employee);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+
+            List<Employee> employeeList = new ArrayList<>();
+
+            String line = bufferedReader.readLine();
+            while (line != null){
+                String[] fields = line.split(",");
+                employeeList.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+                line = bufferedReader.readLine();
+            }
+
+            double sum = employeeList.stream()
+                            .filter(p -> p.getName().charAt(0) == 'M')
+                            .mapToDouble(p -> p.getSalary())
+                            .sum();
+                            //.reduce(0.0, (x, y) -> x + y);
+
+            System.out.print("Enter salary: ");
+            double salary = scanner.nextDouble();
+
+            List<String> emails = employeeList.stream()
+                            .filter(p -> p.getSalary() > salary)
+                            .map(p -> p.getEmail()).sorted()
+                            .collect(Collectors.toList());
+
+            System.out.printf("Email of people whose salary is more than %.2f%n:", salary);
+            emails.forEach(System.out::println);
+
+            System.out.printf("Sum of salary of people whose name starts with 'M': %.2f%n", sum);
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        scanner.close();
     }
-
-
-
-
 }
